d87067b62a2a586cbf70466c3a9c82cf84419cff
sgranado88 <88667162+sgranado88@users.noreply.github.com>
2021-08-12 18:41:15 +0200

    formulario con plantilla terminado

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
   declarations: [
     AppComponent,
     HeroFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+

0b5d35d53a6998883790814e9a525240edd9aab6
sgranado88 <88667162+sgranado88@users.noreply.github.com>
2021-08-13 19:00:16 +0200

    un poco más

--- reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
-export class ProfileEditorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProfileEditorComponent {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+  onSubmit() {
+    console.warn(this.profileForm.value);
   }
-
+  
 }

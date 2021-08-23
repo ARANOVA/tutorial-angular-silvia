8da8a90eabeea806e5768d6083d5c35240eb2c56
Pablo Sanchez <pablo.sanchez@aranova.es>
2021-08-23 19:08:44 +0200

    funcionando

--- angularfire/src/app/app.component.ts
+++ angularfire/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import {AngularFirestore} from '@angular/fire/firestore';
-import { Observable , BehaviorSubject, combineLatest} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable , BehaviorSubject, combineLatest } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import * as firebase from 'firebase/app';
 
 export interface Item {
   text: string;
@@ -15,29 +17,31 @@ export interface Item {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  items: Observable<Item[]>;
-  sizeFilter: BehaviorSubject<string|null>;
-  colorFilter: BehaviorSubject<string|null>;
+  items$: Observable<Item[]>;
+  sizeFilter$: BehaviorSubject<any>;
+  colorFilter$: BehaviorSubject<any>;
 
-  constructor(afs: AngularFirestore){
-    this.sizeFilter = new BehaviorSubject(null);
-    this.colorFilter = new BehaviorSubject(null);
-    this.items= Observable.combineLatest(
-      this.sizeFilter,
-      this.colorFilter
-    ).switchMap(([size, color]) =>
+  constructor(afs: AngularFirestore) {
+    this.sizeFilter$ = new BehaviorSubject(null);
+    this.colorFilter$ = new BehaviorSubject(null);
+    this.items$ = combineLatest<Observable<string | null>[]>(
+      this.sizeFilter$,
+      this.colorFilter$
+    ).pipe(
+      switchMap(([size, color]) =>
         afs.collection<Item>('items', ref=>{
-          let query: firebase.firestore.Query= ref;
+          let query: firebase.default.firestore.Query = ref;
           if (size) {query = query.where('size', '==', size)};
           if (color) {query = query.where('color', '==', color)};
           return query;
         }).valueChanges()
+      )
     );
   }
-  filterBySize (size: string|null){
-    this.sizeFilter.next(size);
+  filterBySize (size: string | null) {
+    this.sizeFilter$.next(size);
   }
-  filterByColor (color: string|null){
-    this.colorFilter.next(color);
+  filterByColor (color: string | null) {
+    this.colorFilter$.next(color);
   }
 }

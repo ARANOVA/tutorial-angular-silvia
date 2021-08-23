a73c8acb74240edab9f107c603aeba17e9cdea27
sgranado88 <88667162+sgranado88@users.noreply.github.com>
2021-08-23 19:07:09 +0200

    menos problemas, pero no se van

--- angularfire/src/app/app.component.ts
+++ angularfire/src/app/app.component.ts
@@ -22,16 +22,18 @@ export class AppComponent {
   constructor(afs: AngularFirestore){
     this.sizeFilter = new BehaviorSubject(null);
     this.colorFilter = new BehaviorSubject(null);
-    this.items= Observable.combineLatest(
+    this.items= combineLatest(
       this.sizeFilter,
       this.colorFilter
-    ).switchMap(([size, color]) =>
+    ).pipe(
+      switchMap(([size, color]) =>
         afs.collection<Item>('items', ref=>{
           let query: firebase.firestore.Query= ref;
           if (size) {query = query.where('size', '==', size)};
           if (color) {query = query.where('color', '==', color)};
           return query;
         }).valueChanges()
+      )
     );
   }
   filterBySize (size: string|null){

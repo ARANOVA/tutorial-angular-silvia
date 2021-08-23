2c9a2446813fde15aae371df9255006e62fe5703
sgranado88 <88667162+sgranado88@users.noreply.github.com>
2021-08-23 18:47:54 +0200

    sigue con errores

--- angularfire/src/app/app.component.ts
+++ angularfire/src/app/app.component.ts
@@ -22,24 +22,22 @@ export class AppComponent {
   constructor(afs: AngularFirestore){
     this.sizeFilter = new BehaviorSubject(null);
     this.colorFilter = new BehaviorSubject(null);
-    this.items= combineLatest(
+    this.items= Observable.combineLatest(
       this.sizeFilter,
       this.colorFilter
-    ).pipe(
-      switchMap(([size, color]) =>
-        afs.collection('items', ref=>{
-          let query: firebase.firestore.CollectionReference | firebase.firestore.Query= ref;
+    ).switchMap(([size, color]) =>
+        afs.collection<Item>('items', ref=>{
+          let query: firebase.firestore.Query= ref;
           if (size) {query = query.where('size', '==', size)};
           if (color) {query = query.where('color', '==', color)};
           return query;
-        }).valueChanges(),
-      )
+        }).valueChanges()
     );
   }
-  filterBySize(size: string|null){
+  filterBySize (size: string|null){
     this.sizeFilter.next(size);
   }
-  filterByColor(color: string|null){
+  filterByColor (color: string|null){
     this.colorFilter.next(color);
   }
 }

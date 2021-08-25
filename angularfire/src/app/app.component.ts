67347431492d54256e746042b9feb046971e7016
Pablo Sánchez <psanchezg@gmail.com>
2021-08-25 13:19:53 +0200

    funcionando de nuevo

--- angularfire/src/app/app.component.ts
+++ angularfire/src/app/app.component.ts
@@ -21,36 +21,48 @@ export class AppComponent {
   items$: Observable<Item[]>;
   sizeFilter$: BehaviorSubject<any>;
   colorFilter$: BehaviorSubject<any>;
-  codeFilter$: BehaviorSubject<any>;
+  codeMinFilter$: BehaviorSubject<any>;
+  codeMaxFilter$: BehaviorSubject<any>;
   participantFilter$: BehaviorSubject<any>;
+  titleFilter$: BehaviorSubject<any>;
 
   @ViewChild('participant') participant!: ElementRef;
-  @ViewChild('code') code!: ElementRef;
+  @ViewChild('codeMin') codeMin!: ElementRef;
+  @ViewChild('codeMax') codeMax!: ElementRef;
 
   constructor(afs: AngularFirestore) {
     this.sizeFilter$ = new BehaviorSubject(null);
     this.colorFilter$ = new BehaviorSubject(null);
-    this.codeFilter$ = new BehaviorSubject(null);
+    this.codeMinFilter$ = new BehaviorSubject(null);
+    this.codeMaxFilter$ = new BehaviorSubject(null);
     this.participantFilter$ = new BehaviorSubject(null);
+    this.titleFilter$ = new BehaviorSubject(null);
     this.items$ = combineLatest<Observable<string | number | null>[]>(
       this.sizeFilter$,
       this.colorFilter$,
-      this.codeFilter$,
-      this.participantFilter$
+      this.codeMinFilter$,
+      this.codeMaxFilter$,
+      this.participantFilter$,
+      this.titleFilter$
     ).pipe(
-      switchMap(([size, color, code, participant, title]) =>
-        afs.collection<Item>('items', ref => {
+      switchMap(([size, color, codeMin, codeMax, participant, title]) => {
+        console.log(size, color, codeMin, codeMax, participant, title);
+        return afs.collection<Item>('items', ref => {
           let query: firebase.default.firestore.Query = ref;
           if (size) { query = query.where('size', '==', size) };
           if (color) { query = query.where('color', '==', color) };
-          console.log("code", code)
-          if (code) { query = query.where('code', '==', code) };
-          console.log("participant", participant)
+          if (codeMin) {
+            query = query.where('code', '>=', parseInt(codeMin as unknown as string));
+          }
+          if (codeMax) {
+            query = query.where('code', '<=', parseInt(codeMax as unknown as string));
+          }
           if (participant) {
             query = query.where('participants', 'array-contains', participant);
           };
           return query;
         }).valueChanges()
+        }
       )
     );
   }
@@ -60,13 +72,21 @@ export class AppComponent {
   filterByColor(color: string | null) {
     this.colorFilter$.next(color);
   }
-  filterByCode(code?: number | null) {
-    if ( code = 0, code <=250){
-        this.codeFilter$.next(code);
-      } else{
-        this.codeFilter$.next(code);
-      } 
+  filterByCode(codeMin?: number | null, codeMax?: number | null) {
+    if (codeMin === undefined) {
+      codeMin = this.codeMin.nativeElement.value;
+    } else {
+      this.codeMin.nativeElement.value = '';
     }
+
+    if (codeMax === undefined) {
+      codeMax = this.codeMax.nativeElement.value;
+    } else {
+      this.codeMax.nativeElement.value = '';
+    }
+    this.codeMinFilter$.next(codeMin);
+    this.codeMaxFilter$.next(codeMax);
+  }
 
   findParticipant(value?: string | null): void {
     if (value === undefined) {
